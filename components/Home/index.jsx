8c850e33cdf3028e7f62948eab8fea92545935ab
Marcos Monte <marcos.monte.junior@gmail.com>
2026-03-27 18:57:01 -0300

    feat: enhance Home component to display dynamic routes with navigation options

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import routes from '../../routes'
 
-export default function Home(){
+const otherRoutes = routes.filter(route => route.name !== 'Tela Inicial')
+
+export default function Home(props){
     return (
         <View>
             <Text>Home</Text>
+            {otherRoutes.map((route, index) => (
+                <TouchableOpacity 
+                    key={index}
+                    onPress={() => { props.navigation.navigate(`${route.name}`)}}
+                >
+                    <Text>{route.name}</Text>
+                </TouchableOpacity>
+            ))}
         </View>
     )
 }
